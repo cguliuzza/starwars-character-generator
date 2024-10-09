c8b577ca05f66941bab42dcc6f1cbd15c9b86018
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-09 13:04:03 -0700

    feat: starwars component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Item from "./Item";
+import Starwars from "./Starwars";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Item name="Simba" />
-        <Item name="Milo" />
-        <Item name="Maisie" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Starwars />
       </header>
     </div>
   );
---
+++ src/Starwars.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Starwars extends React.Component {
+  render() {
+    return <h1>Starwars rules</h1>;
+  }
+}
+
+export default Starwars;
