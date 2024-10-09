4a1a685507d565559ed89bdaac0204fc5c875b1b
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-09 03:43:24 -0700

    feat: state for total remaining

--- src/Item.js
+++ src/Item.js
@@ -5,12 +5,14 @@ class Item extends React.Component {
     super(props);
     this.state = {
       clicks: 0,
+      totalRemaining: 100
     };
   }
 
   clickMe() {
     this.setState({
       clicks: this.state.clicks + 1,
+      totalRemaining: this.state.totalRemaining - 1
     });
   }
 
@@ -19,6 +21,7 @@ class Item extends React.Component {
       <>
         <h1 onClick={() => this.clickMe()}>Hello {this.props.name}</h1>
         <span>Likes: {this.state.clicks}</span>
+        <span>Remaining: {this.state.totalRemaining}</span>
       </>
     );
   }
