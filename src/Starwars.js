3e4975b96ea4c8ae03ef131f762efe2cfe5fe0be
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-09 13:22:41 -0700

    fix: pseudo code

--- src/Starwars.js
+++ src/Starwars.js
@@ -2,7 +2,20 @@ import React from "react";
 
 class Starwars extends React.Component {
   render() {
-    return <h1>Starwars rules</h1>;
+    return (
+      <>
+        <h1>Name</h1>
+        <p>Height (cm)</p>
+        <p>Homeworld</p>
+        <ul>
+          <li>Films</li>
+          <li>Films</li>
+        </ul>
+        <button type="button" className="btn">
+          Generate New Character
+        </button>
+      </>
+    );
   }
 }
 
