438ee3e3ce351ff6e7f31878c4745adae7be013b
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-09 03:38:36 -0700

    feat: state for clicks

--- src/Item.js
+++ src/Item.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+    };
+  }
+
   clickMe() {
-    console.log("I clicked on", this.props.name);
+    this.setState({
+      clicks: this.state.clicks + 1,
+    });
   }
 
   render() {
-    return <h1 onClick={() => this.clickMe()}>Hello {this.props.name}</h1>;
+    return (
+      <>
+        <h1 onClick={() => this.clickMe()}>Hello {this.props.name}</h1>
+        <span>Likes: {this.state.clicks}</span>
+      </>
+    );
   }
 }
 
