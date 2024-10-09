07ac2eb3f580846d0d0dc7066b8bc7f8e4ee3065
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-08 22:16:21 -0700

    feat: click event

--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+const clickMe = () => {
+    console.log("I was clicked!")
+}
+
 class Item extends React.Component {
     render() {
       return (
-        <h1>Hello {this.props.name}</h1>
+        <h1 onClick={() => clickMe()}>Hello {this.props.name}</h1>
       )
     }
 }
