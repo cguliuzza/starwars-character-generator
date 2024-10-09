4c25aed22890f6a00710543bef59a01203c19fab
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-09 03:22:38 -0700

    fix: onClick

--- src/Item.js
+++ src/Item.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-const clickMe = () => {
-    console.log("I was clicked!")
-}
-
 class Item extends React.Component {
-    render() {
-      return (
-        <h1 onClick={() => clickMe()}>Hello {this.props.name}</h1>
-      )
-    }
+  clickMe() {
+    console.log("I clicked on", this.props.name);
+  }
+
+  render() {
+    return <h1 onClick={() => this.clickMe()}>Hello {this.props.name}</h1>;
+  }
 }
 
-export default Item
+export default Item;
