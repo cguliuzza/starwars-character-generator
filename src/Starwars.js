f4788045eaab7109f5bf8be908916219cad90b0d
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-09 20:03:03 -0700

    fix: hardcoded data

--- src/Starwars.js
+++ src/Starwars.js
@@ -12,17 +12,22 @@ class Starwars extends React.Component {
     }
     getNewCharacter() {
         console.log("getNewCharacter")
+        this.setState({
+            name: 'Luke',
+            height: 172,
+            homeworld: 'Tattooine',
+            films: ['1', '2', '3']
+        })
     }
 
   render() {
     return (
       <>
-        <h1>Name</h1>
-        <p>Height (cm)</p>
-        <p>Homeworld</p>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.height}cm</p>
+        <p>Homeworld: {this.state.homeworld}</p>
         <ul>
-          <li>Films</li>
-          <li>Films</li>
+          <li>Films: {this.state.films}</li>
         </ul>
         <button type="button" onClick={() => this.getNewCharacter()} className="btn">
           Generate New Character
