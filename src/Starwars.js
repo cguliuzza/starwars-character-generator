f75e0df38ae40b2494dc2e4f215934b8fe33254c
Tina Guliuzza <christina.guliuzza@gmail.com>
2024-10-09 20:08:12 -0700

    fix: if statement to determine if character has been loaded. Show data when loaded is true.

--- src/Starwars.js
+++ src/Starwars.js
@@ -1,35 +1,46 @@
 import React from "react";
 
 class Starwars extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            name: null,
-            height: null,
-            homeworld: null,
-            films: []
-        }
-    }
-    getNewCharacter() {
-        console.log("getNewCharacter")
-        this.setState({
-            name: 'Luke',
-            height: 172,
-            homeworld: 'Tattooine',
-            films: ['1', '2', '3']
-        })
-    }
+  constructor() {
+    super();
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      height: null,
+      homeworld: null,
+      films: [],
+    };
+  }
+  
+  getNewCharacter() {
+    console.log("getNewCharacter");
+    this.setState({
+      loadedCharacter: true,
+      name: "Luke",
+      height: 172,
+      homeworld: "Tattooine",
+      films: ["1", "2", "3"],
+    });
+  }
 
   render() {
     return (
       <>
-        <h1>{this.state.name}</h1>
-        <p>{this.state.height}cm</p>
-        <p>Homeworld: {this.state.homeworld}</p>
-        <ul>
-          <li>Films: {this.state.films}</li>
-        </ul>
-        <button type="button" onClick={() => this.getNewCharacter()} className="btn">
+        {this.state.loadedCharacter && (
+          <div>
+            <h1>{this.state.name}</h1>
+            <p>{this.state.height}cm</p>
+            <p>Homeworld: {this.state.homeworld}</p>
+            <ul>
+              <li>Films: {this.state.films}</li>
+            </ul>
+          </div>
+        )}
+        <button
+          type="button"
+          onClick={() => this.getNewCharacter()}
+          className="btn"
+        >
           Generate New Character
         </button>
       </>
